bc4b341204208f24bbb4cc5be996235562ba402d
Will <billyjoe2701@gmail.com>
2023-12-12 11:00:16 +0000

    Adds messagge for no comments

--- nc-news-fe/src/components/CommentContent.jsx
+++ nc-news-fe/src/components/CommentContent.jsx
@@ -1,30 +1,37 @@
-import { useState } from "react"
-import CommentCard from "./CommentCard"
+import { useState } from "react";
+import CommentCard from "./CommentCard";
 
-const CommentContent = ({comments}) => {
+const CommentContent = ({ comments }) => {
+  const [comment, setComment] = useState("");
+  const [showComments, setShowComments] = useState(false);
 
-    const [comment, setComment] = useState("")
-    const [showComments, setShowComments] = useState(false)
+  return (
+    <section>
+      <form className="d-flex flex-column align-items-center">
+        <div className="d-flex">
+          <label htmlFor="comment">
+            <input
+              type="text"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          </label>
+          <button>Add</button>
+        </div>
+      </form>
+      <h2>Most Recent Comment</h2>
+      <button onClick={() => setShowComments(true)}>View All Comments</button>
+      {comments.length > 0 && showComments ? (
+        <div>
+          {comments.map((comment) => (
+            <CommentCard key={comment.comment_id} comment={comment} />
+          ))}
+        </div>
+      ) : showComments ? (
+        <p>No Comments</p>
+      ) : null}
+    </section>
+  );
+};
 
-    return (
-            <section>
-        <form className="d-flex flex-column align-items-center">
-            <div className="d-flex">
-            <label htmlFor="comment">
-                <input type="text" value={comment} onChange={(e)=>setComment(e.target.value)}/>
-            </label>
-            <button>Add</button>
-            </div>
-        </form>
-            <h2>Most Recent Comment</h2>
-            <button onClick={()=> setShowComments(true)}>View All Comments</button>
-            {showComments ? (
-                comments.map(comment => {
-                    return <CommentCard key={comment.comment_id} comment={comment} />
-                })
-            ): <></>}
-            </section>
-    )
-}
-
-export default CommentContent
+export default CommentContent;
